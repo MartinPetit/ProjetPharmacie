1a19bba03fd980552d3dcbd4f9541c60399a2cfe
MartinPetit <martinpetit1998@gmail.com>
2019-10-31 15:03:55 +0100

    ajout du formualaire de contact + envoi de mail

--- src/Controller/homeController.php
+++ src/Controller/homeController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class homeController extends AbstractController {
 
@@ -18,7 +26,68 @@ class homeController extends AbstractController {
     }
 
 
+     /**
+     * Permet d'avoir la configuration de base d'un champ
+     * 
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    
+    private function getConfiguration($label, $placeholder) {
+        return  [
+            'label' => $label, 
+        'attr' => [
+            'placeholder' => $placeholder
+        ]
+        ];
+    }
+    
+    
 
+	 /**
+     * @Route("/", name="homepage")
+     */
+
+
+    public function test(Request $request, \Swift_Mailer $mailer) {
+
+            $form = $this->createFormBuilder()
+            ->add('name', TextType::class, $this->getConfiguration("Nom", "Entrer votre nom de famille"))
+            ->add('firstname', TextType::class, $this->getConfiguration("Prénom", "Entrer votre prénom"))
+            ->add('mail', EmailType::class, $this->getConfiguration("Mail", "Entrer votre mail"))
+            ->add('message', TextareaType::class, $this->getConfiguration("Message", "Entrer votre message"))
+            ->getForm()
+            ;
+    
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $ourFormData = $form->getData();
+
+                $message = (new \Swift_Message('Hello Mail'))
+                    ->setFrom([$ourFormData['mail'] => $ourFormData['mail']] )
+                    ->setTo('martinpetit1998@gmail.com')
+                    ->setBody(
+                        $ourFormData['message']
+                    ,  'text/plain'
+                );
+
+                $mailer->send($message);
+
+                $this->addFlash(
+                    'success', 
+                    "Votre message a bien été envoyé"
+                  );
+                  
+                  return $this->redirectToRoute('homepage');
+            }
+    
+            return $this->render('Home/home.html.twig', [
+                'form' => $form->createView()
+            ]);
+
+            }
 
 
 
